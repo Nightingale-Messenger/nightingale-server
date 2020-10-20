d25f8775a726adf0b71b5389b479bfa22c458d9e
Taras <taras2178@gmail.com>
2020-10-20 17:44:51 +0300

    add chat functions

--- API/Hubs/UserHub.cs
+++ API/Hubs/UserHub.cs
@@ -1,16 +1,61 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
 using API.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Hubs
 {
     class UserHub : Hub
     {
-        [Authorize]
-        public async Task Send(string message, string to)
+        private readonly DatabaseContext _dbContext;
+        private readonly UserManager<User> _userManager;
+
+        private UserHub(DatabaseContext db,
+            UserManager<User> userManager)
         {
-            await Clients.User(to).SendAsync("ReceiveMessage", message);
+            _dbContext = db;
+            _userManager = userManager;
+        }
+        
+        [Authorize]
+        public async Task Send(ChatMessage msg)
+        {
+            var newMessage = new Message()
+            {
+                DateTime = msg.Date,
+                Receiver = await _userManager.FindByIdAsync(Convert.ToString(msg.ReceiverId)),
+                Text = msg.Text,
+                User = await _userManager.FindByIdAsync(Convert.ToString(msg.SenderId))
+            };
+            await _dbContext.Messages.AddAsync(newMessage);
+            await _dbContext.SaveChangesAsync();
+            await Clients.User(Convert.ToString(msg.ReceiverId)).SendAsync("ReceiveMessage", newMessage);
+        }
+        
+        [Authorize]
+        public async Task GetLastMessages(string receiverId)
+        {
+            var receiver = await _userManager.FindByIdAsync(receiverId);
+            var currUser = await _userManager.GetUserAsync(Context.User);
+            await Clients.Caller.SendAsync("GetMessages",
+                _dbContext.Messages.Where(m => m.Receiver == receiver &&
+                                               m.User == currUser).Take(20));
+        }
+
+        public async Task GetMessagesBefore(string receiverId, int messageId)
+        {
+            var receiver = await _userManager.FindByIdAsync(receiverId);
+            var currUser = await _userManager.GetUserAsync(Context.User);
+
+            await Clients.Caller.SendAsync("GetMessages",
+                _dbContext.Messages.Where(m => m.Receiver == receiver &&
+                                               m.User == currUser &&
+                                               m.Id < messageId).Take(20));
         }
     }
 }
---
+++ API/Models/ChatMessage.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace API.Models
+{
+    public class ChatMessage
+    {
+        public Guid SenderId { get; set; }
+        
+        public Guid ReceiverId { get; set; }
+        
+        public string Text { get; set; }
+        
+        public DateTime Date { get; set; }
+    }
+}
